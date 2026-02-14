10a8e7d9525e681f621d1658735679e7e52616a8
Terrence Zhang <zhangt@microsoft.com>
2026-02-13 18:36:11 -0800

    Upgrade nvbit to 1.7.7.1 and update cutlass_volta shapes
    - install_nvbit.sh: upgrade from v1.7.4 to v1.7.7.1 (CUDA 13.0 support)
    - Handle renamed extraction dir (nvbit_release_x86_64 -> nvbit_release)
    - define-all-apps.yml: simplify cutlass_volta to single 16x576x7168 shape

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
-rm -rf $BASH_ROOT/nvbit_release
-wget https://github.com/NVlabs/NVBit/releases/download/v1.7.4/nvbit-Linux-x86_64-1.7.4.tar.bz2
-tar -xf nvbit-Linux-x86_64-1.7.4.tar.bz2 -C $BASH_ROOT
-rm nvbit-Linux-x86_64-1.7.4.tar.bz2
+rm -rf $BASH_ROOT/nvbit_release $BASH_ROOT/nvbit_release_x86_64
+wget https://github.com/NVlabs/NVBit/releases/download/v1.7.7.1/nvbit-Linux-x86_64-1.7.7.1.tar.bz2
+tar -xf nvbit-Linux-x86_64-1.7.7.1.tar.bz2 -C $BASH_ROOT
+rm nvbit-Linux-x86_64-1.7.7.1.tar.bz2
+# v1.7.7.1 extracts to nvbit_release_x86_64, rename for compatibility
+if [ -d "$BASH_ROOT/nvbit_release_x86_64" ] && [ ! -d "$BASH_ROOT/nvbit_release" ]; then
+    mv $BASH_ROOT/nvbit_release_x86_64 $BASH_ROOT/nvbit_release
+fi
